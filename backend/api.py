c0f3e383fdacfd3b1dda14930fa8d39106de78cc
Ihit-Joshi <joshiihitc@gmail.com>
2026-02-08 22:45:24 +0530

    Temp isolate import crash for Render

--- backend/api.py
+++ backend/api.py
@@ -1,8 +1,10 @@
 import os
 from app_init import app, logger
 
-# Import modules to register their routes and handlers
-import errors      # registers error handlers
-from routes import auth_routes
-from routes import search_routes
-from routes import stock_routes
+# # Import modules to register their routes and handlers
+# import errors      # registers error handlers
+# from routes import auth_routes
+# from routes import search_routes
+# from routes import stock_routes
+
+from app_init import app
